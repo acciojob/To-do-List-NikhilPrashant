0d0be43cae8d6dc72eb8c0d8a2a5a7db86876841
Nikhil Prashant <nikhilprashant1@gmail.com>
2023-04-20 15:08:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
+const newTodoInput = document.getElementById('newTodoInput');
+const addTodoButton = document.getElementById('addTodoButton');
+const todoList = document.getElementById('todoList');
 
+addTodoButton.addEventListener('click', function() {
+	const todoNext = newTodoInput.value.trim();
+	if (todoNext != '') {
+		const newTodo = document.createElement('li');
+		newTodo.innerText = todoNext;
+		todoList.appendChild(newTodo);
+		newTodoInput.value = '';
+	}
+});
